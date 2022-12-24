95901fc2d4ce0e164a61eef30d31faacc1942fdb
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-12-25 00:32:13 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div flex justify-center sm:justify-end items-center h="100vh" class="container" mx-auto>
         <Car position-absolute bottom-0 left-0 h="75vh" hidden lg:block />
         <div w="full" flex flex-col px="20px lg:70px" dir="rtl" items-center justify-center lg:items-start lg:justify-start>
-            <img src="logo.png" h="200px" w="216px" alt="" />
+            <img src="/logo.png" h="200px" w="216px" alt="" />
             <h1 text="64px lg:85px">خطوط</h1>
             <p text="24px lg:30px center lg:right">
                 <span>تطبيقك الأول للحصول على خطوط</span>
